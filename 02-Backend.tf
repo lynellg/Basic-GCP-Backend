5d3045db80882b3f48fc71d58a7c85005c8bb81a
lynellg <lifeoflynell@gmail.com>
2025-08-07 18:48:38 -0400

    added README to basic gcp backend

--- 02-Backend.tf
+++ 02-Backend.tf
@@ -1,15 +1,16 @@
+#https://registry.terraform.io/providers/hashicorp/google/latest
 terraform {
   required_providers {
     google = {
-      source = "hashicorp/google"
+      source  = "hashicorp/google"
       version = "6.47.0"
     }
   }
 }
 
 provider "google" {
-    credentials = "Class65main.json"
-    project     = "Jody65"
-    region      = "us-central1"
+  credentials = "Class65main.json"
+  project     = "Jody65"
+  region      = "us-central1"
   # Configuration options
 }
---
+++ README.md
@@ -0,0 +1,5 @@
+In this repo I have demonstrated that I can set up a Backend file in GCP using Terraform and push the code to github.
+
+I used the required Terraform & Google required providers resource blocks in terraform
+
+I also added a .gitignore file to prevent any tfstate and json files from being pushed to github
